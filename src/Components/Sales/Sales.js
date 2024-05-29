352977b4829392e2aa6db1d0f19b08eb93429e8e
SoyFroylanMtz <soyfroylanmtzcarlos@gmail.com>
2024-05-29 12:06:53 -0600

    mensaje

--- src/Components/Sales/Sales.js
+++ src/Components/Sales/Sales.js
@@ -1,58 +1,52 @@
-import React,  { useState } from 'react'
+import React, { useState } from 'react';
 import { searchProductById } from '../../services/apiSaleService';
+import ModalCode from '../ModalCode/ModalCode';
 import './Sales.css';
 import { MdOutlineSegment } from "react-icons/md";
 import { FaSearch } from 'react-icons/fa';
 import { IoTrashOutline } from "react-icons/io5";
-import { FaMoneyCheck } from "react-icons/fa6";
+import { FaMoneyCheck } from "react-icons/fa";
 import { BsCashCoin } from "react-icons/bs";
 import { FaCcMastercard } from "react-icons/fa";
 import EcoVentas from "../../img/Eco-Ventas.png";
 
-
-
 function Sales() {
-
     const [searchTerm, setSearchTerm] = useState('');
     const [selectedRow, setSelectedRow] = useState(null);
     const [selectedProduct, setSelectedProduct] = useState(null);
     const [selectedProducts, setSelectedProducts] = useState([]);
+    const [showModal, setShowModal] = useState(false);
 
-
-
-    
-
-
-    //AQUI BUSCA A ELEMENTO A TRAVES DEL BUSCADOS
     const handleSearchSubmit = async (event) => {
         event.preventDefault();
         try {
             const result = await searchProductById(searchTerm);
             if (result.status === 200 && result.data) {
-                // Buscar si el producto ya está en la lista
-                const index = selectedProducts.findIndex(product => product.id === result.data.id);
-    
-                if (index !== -1) {
-                    // Producto encontrado, incrementar cantidad
-                    const newProducts = [...selectedProducts];
-                    newProducts[index] = {
-                        ...newProducts[index],
-                        quantity: newProducts[index].quantity + 1,
-                        total: (newProducts[index].quantity + 1) * newProducts[index].price
-                    };
-                    setSelectedProducts(newProducts);
+                const product = result.data;
+                if (product.is_active) {
+                    const index = selectedProducts.findIndex(p => p.id === product.id);
+                    if (index !== -1) {
+                        const newProducts = [...selectedProducts];
+                        newProducts[index] = {
+                            ...newProducts[index],
+                            quantity: newProducts[index].quantity + 1,
+                            total: (newProducts[index].quantity + 1) * newProducts[index].price
+                        };
+                        setSelectedProducts(newProducts);
+                    } else {
+                        const newProduct = {
+                            id: product.id,
+                            name: product.name,
+                            imageSrc: product.image,
+                            category: product.clasification.name,
+                            price: parseFloat(product.price_sale),
+                            quantity: 1,
+                            total: parseFloat(product.price_sale)
+                        };
+                        setSelectedProducts(prevProducts => [...prevProducts, newProduct]);
+                    }
                 } else {
-                    // Producto no encontrado, agregar nuevo
-                    const newProduct = {
-                        id: result.data.id,
-                        name: result.data.name,
-                        imageSrc: result.data.image,
-                        category: result.data.clasification.name,
-                        price: parseFloat(result.data.price_sale),
-                        quantity: 1,
-                        total: parseFloat(result.data.price_sale)
-                    };
-                    setSelectedProducts(prevProducts => [...prevProducts, newProduct]);
+                    setShowModal(true);  // Mostrar el modal si el producto no está activo
                 }
                 setSearchTerm('');
             } else {
@@ -62,21 +56,19 @@ function Sales() {
             console.error('Error al buscar producto:', error);
         }
     };
-    
 
-    //ELIMINA EL ELEMENTO DE LA TABLA
     const handleDeleteClick = (productId) => {
-        // Lógica para eliminar el producto seleccionado
         const filteredProducts = selectedProducts.filter(product => product.id !== productId);
         setSelectedProducts(filteredProducts);
-        // Si el producto eliminado está actualmente seleccionado, limpiar la selección
         if (selectedProduct && selectedProduct.id === productId) {
             setSelectedProduct(null);
-            setSelectedRow(null); // Opcional, si deseas también limpiar la selección visual en la tabla
+            setSelectedRow(null);
         }
     };
 
-
+    const closeModal = () => {
+        setShowModal(false);
+    };
 
     const handleSearchChange = (event) => {
         setSearchTerm(event.target.value);
@@ -87,7 +79,6 @@ function Sales() {
         setSelectedProduct(selectedProducts[index]);
     };
 
-    
     const handleRestar = () => {
         if (selectedProduct && selectedProduct.quantity > 1) {
             const newProducts = selectedProducts.map(product => {
@@ -101,8 +92,6 @@ function Sales() {
                 return product;
             });
             setSelectedProducts(newProducts);
-    
-            // Opcionalmente actualiza el selectedProduct para reflejar el cambio directamente en los controles de la UI
             setSelectedProduct({
                 ...selectedProduct,
                 quantity: selectedProduct.quantity - 1,
@@ -110,7 +99,6 @@ function Sales() {
             });
         }
     };
-    
 
     const handleSumar = () => {
         if (selectedProduct) {
@@ -125,8 +113,6 @@ function Sales() {
                 return product;
             });
             setSelectedProducts(newProducts);
-            
-            // Opcionalmente actualiza el selectedProduct para reflejar el cambio directamente en los controles de la UI
             setSelectedProduct({
                 ...selectedProduct,
                 quantity: selectedProduct.quantity + 1,
@@ -134,43 +120,42 @@ function Sales() {
             });
         }
     };
-    
 
-  return (
-    <div className='sales-container'>
-        <div className='sales-container-section1'>
-            <div className="stock-cabecera">
-                <MdOutlineSegment className="stock-icono-classname-lines" />
-                <div className="stock-title">
-                    <h2 className="stock-header">Ventas</h2>
-                </div>
-                <div className="sales-search-container">
-                    <form onSubmit={handleSearchSubmit} className="sales-search-form">
-                    <div className="sales-search-input-container">
-                        <FaSearch className="sales-search-icon"/>
-                        <input 
-                            type="text" 
-                            className="sales-search-input" 
-                            placeholder="Busca tu producto" 
-                            value={searchTerm}
-                            onChange={handleSearchChange} 
-                        />
+    return (
+        <div className='sales-container'>
+            <div className='sales-container-section1'>
+                <div className="stock-cabecera">
+                    <MdOutlineSegment className="stock-icono-classname-lines" />
+                    <div className="stock-title">
+                        <h2 className="stock-header">Ventas</h2>
                     </div>
-                    </form>
-                    <div className="sales-buscar">
-                        <button className="sales-search-button" >Buscar</button>
+                    <div className="sales-search-container">
+                        <form onSubmit={handleSearchSubmit} className="sales-search-form">
+                            <div className="sales-search-input-container">
+                                <FaSearch className="sales-search-icon" />
+                                <input
+                                    type="text"
+                                    className="sales-search-input"
+                                    placeholder="Busca tu producto"
+                                    value={searchTerm}
+                                    onChange={handleSearchChange}
+                                />
+                            </div>
+                        </form>
+                        <div className="sales-buscar">
+                            <button className="sales-search-button">Buscar</button>
+                        </div>
                     </div>
-                </div>
 
-                <div className='sales-table-fondo'>
-                    <div className="sales-table-header">
-                        <div className='sales-table-nombre'>Nombre</div>
-                        <div className='sales-table-precio'>Precio</div>
-                        <div className='sales-table-cantidad'>Cantidad</div>
-                        <div className='sales-table-total'>Total</div>
-                    </div>
-                    <div className="sales-table-container">
-                        <table className="sales-table">
+                    <div className='sales-table-fondo'>
+                        <div className="sales-table-header">
+                            <div className='sales-table-nombre'>Nombre</div>
+                            <div className='sales-table-precio'>Precio</div>
+                            <div className='sales-table-cantidad'>Cantidad</div>
+                            <div className='sales-table-total'>Total</div>
+                        </div>
+                        <div className="sales-table-container">
+                            <table className="sales-table">
                                 <tbody>
                                     {selectedProducts.map((product, index) => (
                                         <tr
@@ -185,82 +170,56 @@ function Sales() {
                                         </tr>
                                     ))}
                                 </tbody>
-                            
-                        </table>
+                            </table>
+                        </div>
                     </div>
                 </div>
-
-
-
             </div>
 
-        </div>
-
-
-
-
-
-        <div className='sales-container-section2'>
-
-
-            <div className='sales-container-producto-select'>
-                {!selectedProduct ?(
-                    <div className="sales-welcome-message">
-                        <img src={EcoVentas} alt="Bienvenido" className="sales-welcome-image" />
-                        <p>Bienvenido a una nueva venta con EcoPym</p>
-                    </div>
-                ):(
-                    <>
-
-                <div className='sale-producto-img-info'>
-                    <div>
-                        <img className="sale-products-img" src={selectedProduct ? selectedProduct.imageSrc : ''} alt="Producto seleccionado" />
-                    </div>
-                    <div className="sale-products-name">
-                        {selectedProduct ? selectedProduct.name : 'Seleccione un producto'}
-                        <div className="sale-products-price">
-                            {selectedProduct ? `$${selectedProduct.price.toFixed(2)}` : ''}
+            <div className='sales-container-section2'>
+                <div className='sales-container-producto-select'>
+                    {!selectedProduct ? (
+                        <div className="sales-welcome-message">
+                            <img src={EcoVentas} alt="Bienvenido" className="sales-welcome-image" />
+                            <p>Bienvenido a una nueva venta con EcoPym</p>
                         </div>
-                        <div className="sale-product-category">
-                            {selectedProduct ? selectedProduct.category : ''}
-                        </div>
-                        <div className='sale-products-delete-container'>
-                            {selectedProduct && (
-                                <button className="sale-products-delete" onClick={() => handleDeleteClick(selectedProduct.id)}>
-                                    <IoTrashOutline />
-                                </button>
-                            )}
-                        </div>
-                    </div>
-                </div>
-                <div className='sales-quantity-controls-container '>
-                    {selectedProduct && (
-                        <div className="sales-quantity-controls">
-                            <div className="sales-quantity-buttons">
-                                <button className="sales-quantity-button" onClick={handleRestar}>-</button>
-                                <div className="sales-quantity-display">{selectedProduct.quantity}</div>
-                                <button className="sales-quantity-button" onClick={handleSumar}>+</button>
+                    ) : (
+                        <>
+                            <div className='sale-producto-img-info'>
+                                <div>
+                                    <img className="sale-products-img" src={selectedProduct ? selectedProduct.imageSrc : ''} alt="Producto seleccionado" />
+                                </div>
+                                <div className="sale-products-name">
+                                    {selectedProduct ? selectedProduct.name : 'Seleccione un producto'}
+                                    <div className="sale-products-price">
+                                        {selectedProduct ? `$${selectedProduct.price.toFixed(2)}` : ''}
+                                    </div>
+                                    <div className="sale-product-category">
+                                        {selectedProduct ? selectedProduct.category : ''}
+                                    </div>
+                                    <div className='sale-products-delete-container'>
+                                        {selectedProduct && (
+                                            <button className="sale-products-delete" onClick={() => handleDeleteClick(selectedProduct.id)}>
+                                                <IoTrashOutline />
+                                            </button>
+                                        )}
+                                    </div>
+                                </div>
                             </div>
-                        </div>
+                            <div className='sales-quantity-controls-container'>
+                                {selectedProduct && (
+                                    <div className="sales-quantity-controls">
+                                        <div className="sales-quantity-buttons">
+                                            <button className="sales-quantity-button" onClick={handleRestar}>-</button>
+                                            <div className="sales-quantity-display">{selectedProduct.quantity}</div>
+                                            <button className="sales-quantity-button" onClick={handleSumar}>+</button>
+                                        </div>
+                                    </div>
+                                )}
+                            </div>
+                        </>
                     )}
                 </div>
-
-
-
-
-                </>
-                )}
-                        
-                        
-
-
-
-
-
-                </div>
-
-
-
 
                 <div className='sales-container-producto-info'>
                     <div className="sales-summary">
@@ -272,7 +231,7 @@ function Sales() {
                             <span>Subtotal</span>
                             <span>$380.90</span>
                         </div>
-                        <div className="sales-summary-item  iva-item">
+                        <div className="sales-summary-item iva-item">
                             <span>IVA</span>
                             <span>$12.78</span>
                         </div>
@@ -285,29 +244,28 @@ function Sales() {
                         <span>Payment Method</span>
                         <div className="payment-method-icons">
                             <button className="payment-method-button">
-                            <BsCashCoin />
-
+                                <BsCashCoin />
                             </button>
                             <button className="payment-method-button">
-                            <FaMoneyCheck />
+                                <FaMoneyCheck />
                             </button>
                             <button className="payment-method-button">
-                            <FaCcMastercard />
-
+                                <FaCcMastercard />
                             </button>
                         </div>
                     </div>
                     <button className="pay-button">COBRAR</button>
                 </div>
+            </div>
 
-            
-
-
-
+            {showModal && (
+                <ModalCode onClose={closeModal} 
+                title="El producto no está activo"
+                msg="Ingresa el precio de venta"
+                />  // Mostrar el componente ModalCode cuando showModal es true
+            )}
         </div>
-      
-    </div>
-  )
+    );
 }
 
-export default Sales
+export default Sales;
